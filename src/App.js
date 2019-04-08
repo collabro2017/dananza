faed85960e762edc8f0bff196fd3fe568e550b11
ODIN Labs <collabro2017@gmail.com>
2019-04-08 05:22:01 +0300

    About page and Help page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import { connect } from "react-redux";
 import Home from "./containers/Home";
 import Setting from "./containers/Setting";
 import Signin from "./containers/Signin";
+import About from "./containers/About";
+import Help from "./containers/Help";
 
 import MainLayout from "./layouts/MainLayout";
 import EmptyLayout from "./layouts/EmptyLayout";
@@ -63,6 +65,8 @@ class App extends Component {
               <Switch>
                 <DashboardRoute path="/dashboard" component={Home} />
                 <DashboardRoute path="/setting" component={Setting} />
+                <DashboardRoute path="/about" component={About} />
+                <DashboardRoute path="/help" component={Help} />
                 <Route path="/signin" render={() => <Redirect to="/" />} />
                 <DashboardRoute exact path="/" component={Home} />
                 <EmptyRoute component={NotFound} />
