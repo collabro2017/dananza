181eef3d8c03f15b0a3b51b6a31432652f6fc20a
ODIN Labs <collabro2017@gmail.com>
2019-04-25 20:37:33 +0300

    Email check when signUp and Fix response of signin

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,21 +7,35 @@ const User = require('../models').User;
 
 router.post('/signup', function(req, res) {
   if (!req.body.email || !req.body.password) {
-    res.status(400).send({msg: 'Please pass email and password.'})
+    res.status(400).send({message: 'Please pass email and password.'})
   } else {
     User
-      .create({
-        email: req.body.email,
-        password: req.body.password,
-        f_name: req.body.firstName,
-        l_name: req.body.lastName,
-        business_name: req.body.businessName
+      .find({
+        where: {
+          email: req.body.email
+        }
       })
-      .then((user) => res.status(201).send(user))
-      .catch((error) => {
-        console.log(error);
-        res.status(400).send(error);
-      });
+      .then((user) => {
+        if (!user) {
+          User
+            .create({
+              email: req.body.email,
+              password: req.body.password,
+              f_name: req.body.firstName,
+              l_name: req.body.lastName,
+              business_name: req.body.businessName
+            })
+            .then((user) => res.status(201).send(user))
+            .catch((error) => {
+              console.log(error);
+              res.status(400).send(error);
+            });
+        }
+        else{
+            res.status(400).send({success: false, message: 'Email already exists.'})
+        }
+      })
+      .catch((error) => res.status(400).send(error));    
   }
 });
 
@@ -44,9 +58,9 @@ router.post('/signin', function(req, res) {
             jwt.verify(token, 'nodeauthsecret', function(err, data){
               console.log(err, data);
             })
-            res.json({success: true, token: 'JWT ' + token});
+            res.json({success: true, user_info:user, token: 'JWT ' + token});
           } else {
-            res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+            res.status(401).send({success: false, message: 'Authentication failed. Wrong password.'});
           }
         })
       })
